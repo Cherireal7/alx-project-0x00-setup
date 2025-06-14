6f2463833f315622050aff62badc77e94f8d8182
Cherinet <cheridemeke7@gmail.com>
2025-06-14 12:13:53 +0300

    finalfinal

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,23 +1,16 @@
 import Card from "@/components/Card";
-import Button from "@/components/Button";
+import ButtonVariants from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div className="p-6">
       <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
 
-      <div className="flex flex-wrap gap-4 mb-6">
-        <Button title="Small" styles="text-xs" />
-        <Button title="Medium" styles="text-base" />
-        <Button title="Large" styles="text-xl" />
+      <ButtonVariants />
 
-        <Button title="Rounded-sm" styles="rounded-sm" />
-        <Button title="Rounded-md" styles="rounded-md" />
-        <Button title="Rounded-lg" styles="rounded-lg" />
-        <Button title="Rounded-full" styles="rounded-full" />
+      <div className="mt-8">
+        <Card />
       </div>
-
-      <Card />
     </div>
   );
 };
